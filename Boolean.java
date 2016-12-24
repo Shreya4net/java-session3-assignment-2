4af07ce7727d25f1dc76fc9b662bfe577229f198
Shreya4net <pujakumari.civil@gmail.com>
2016-12-24 12:39:01 +0530

    java session3 assignment 2
    java session3 assignment 2

---
+++ Boolean.java
@@ -0,0 +1,21 @@
+package session;
+
+public class Boolean {
+	public static void main(String[] args){
+	boolean same =true;
+	String st = " I love pizza";
+	String st1 ="ccccc" ;
+	{
+		if(same){
+			st=st1;
+			
+			System.out.println(st);
+			
+		}else 
+			same=false;
+		
+		}
+	}
+}
+
+
